4cac8ad54590fba7bc5f88eed64ae7562efe0de0
Matthias Giger <i@matthiasgiger.com>
2023-10-24 23:30:56 +0200

    feat(bin): script to translate sheet during build
    release-npm

---
+++ bin.ts
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+import minimist from 'minimist'
+import { existsSync, lstatSync, readFileSync, writeFileSync } from 'fs'
+import { join, dirname, isAbsolute } from 'path'
+import { log } from './helper'
+import { Language } from './types'
+import { translate } from './translate'
+
+const { input, output, language = Language.en } = minimist(process.argv.slice(2))
+let fullInputPath = isAbsolute(input) ? input : join(process.cwd(), input)
+let fullOutputPath = isAbsolute(output) ? output : join(process.cwd(), output)
+
+if (!existsSync(fullInputPath)) {
+  log(`Input file "${input}" does not exist`, 'error')
+}
+
+// Check if input is a folder and add en.json in this case
+if (lstatSync(fullInputPath).isDirectory()) {
+  fullInputPath = join(fullInputPath, 'en.json')
+}
+
+if (!output) {
+  fullOutputPath = dirname(input)
+}
+
+if (!existsSync(fullOutputPath)) {
+  log(`Output folder "${output}" does not exist`, 'error')
+}
+
+if (!lstatSync(fullOutputPath).isDirectory()) {
+  log(`Output "${output}" must be a directory`, 'error')
+}
+
+if (!(language in Language)) {
+  log(`Invalid language "${language}" passed`, 'error')
+}
+
+const languages = Object.keys(Language)
+// Remove input language.
+languages.splice(languages.indexOf(language), 1)
+
+const inputSheetContents = readFileSync(fullInputPath, 'utf8')
+const inputSheet = JSON.parse(inputSheetContents)
+
+const translatePromises = languages.map(async (currentLanguage: Language) => {
+  const sheet = await translate(JSON.stringify(inputSheet), currentLanguage)
+  writeFileSync(join(fullOutputPath, `${currentLanguage}.json`), JSON.stringify(sheet, null, 2), {
+    flag: 'w', // Override existing file.
+  })
+})
+
+await Promise.all(translatePromises)
