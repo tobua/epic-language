c494b66c7f308a7826c2d514938ef28d751e560d
Matthias Giger <i@matthiasgiger.com>
2024-02-03 12:14:10 +0100

    fix(demo): resolve issues specific to Serverless function runtime

--- demo/api/static/edge/[lang].ts
+++ demo/api/static/edge/[lang].ts
@@ -9,10 +9,7 @@ export async function GET(request: Request) {
   const searchParams = new URL(request.url).searchParams
   const language = searchParams.get('lang') ?? ''
   if (!(language in Language)) return new Response(`Missing language "${language}"`)
-  const {
-    error,
-    value: { default: sheet },
-  } = await it(import(`../translations/${language}.json`))
+  const { error, value: sheet } = await it(import(`../translations/${language}.json`))
   if (error) return new Response(`Sheet for language "${language}" not found!`)
   return new Response(JSON.stringify(sheet), {
     status: 200,
--- demo/api/static/serverless/[lang].ts
+++ demo/api/static/serverless/[lang].ts
@@ -6,10 +6,7 @@ export default async function handler(request: VercelRequest, response: VercelRe
   const language = request.query.lang as Language
   if (!(language in Language))
     return response.status(500).json({ error: `Missing language "${language}"` })
-  const {
-    error,
-    value: { default: sheet },
-  } = await it(import(`../translations/${language}.json`))
+  const { error, value: sheet } = await it(import(`../translations/${language}.json`))
   if (error)
     return response.status(500).json({ error: `Sheet for language "${language}" not found!` })
   response.status(200).json(sheet)
